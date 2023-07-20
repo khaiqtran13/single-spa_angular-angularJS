e13e484e80c394e964474bb4c98ee315488d69ef
khaitran <khaitran@cisco.com>
2023-07-20 16:33:40 -0400

    baseline angular and angularjs single-spa

---
+++ main.js
@@ -0,0 +1,67 @@
+// creation through System
+System.register([], function (_export) {
+  return {
+    execute: function () {
+      _export(
+        (window.legacyAngularApp = window.singleSpaAngularjs.default({
+          angular: angular,
+          mainAngularModule: "main-module",
+          uiRouter: true,
+          perserveGlobal: false,
+        }))
+      );
+    },
+  };
+});
+
+// creation through Window
+// window.legacyAngularApp = window.singleSpaAngularjs.default({
+//   angular: angular,
+//   mainAngularModule: "main-module",
+//   uiRouter: true,
+//   perserveGlobal: false,
+// });
+
+var app = angular.module("main-module", ["ui.router"]);
+
+app.config(function ($stateProvider) {
+  $stateProvider.state({
+    name: "main",
+    url: "/home",
+    template: "<home />",
+  });
+});
+
+app.component("home", {
+  template: `First Name: <input type="text" ng-model="firstName" ng-change="vm.log()"><br>
+    Last Name: <input type="text" ng-model="lastName"><br>
+    <br>
+    Full Name: {{firstName + ' ' +  lastName }}`,
+  controllerAs: "vm",
+  controller: function () {
+    var vm = this;
+
+    vm.$onInit = function () {
+      console.log("mounting angularjs component");
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+    };
+
+    vm.$onDestroy = function () {
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+      console.log("unmounting angularjs component!");
+    };
+
+    vm.log = function () {
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+    };
+  },
+});
